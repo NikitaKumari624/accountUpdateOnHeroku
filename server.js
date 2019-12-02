661bcb9156c07347884f681bc29a8c98014cdc2e
NikitaKumari624 <57701589+NikitaKumari624@users.noreply.github.com>
2019-12-02 16:30:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,8 +12,10 @@ app.use(bodyParser.json());
 app.post('/update', function(req, res) {
     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
         console.log('process db url---'+process.env.DATABASE_URL);
+        
         // watch for any connect issues
         if (err) console.log(err);
+        res.send(err);
         conn.query(
             'UPDATE salesforce.Contact SET Phone = $1, MobilePhone = $1 WHERE LOWER(FirstName) = LOWER($2) AND LOWER(LastName) = LOWER($3) AND LOWER(Email) = LOWER($4)',
             [req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim()],
